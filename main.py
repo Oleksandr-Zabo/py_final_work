985784f8545ad3bb929a735bc6b14397ac0f752b
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-02-10 16:07:16 +0200

    #2 sum of range #3 sum 1 to 100, %2

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 print("Hello python exam")
 
+# Частина 1
 # 1
 print("Привіт, світ!")  # print("Hello world")
+
+# 2
+f_numb2 = int(input("Введи перше число: "))
+s_numb2 = int(input("Введи друге число: "))
+sum_range2 = 0
+for i in range(f_numb2 + 1, s_numb2):
+    sum_range2 += i
+print(f"Сума діапазону чисел : {sum_range2}")
+
+# 3
+sum_range3 = 0
+for i in range(1, 100+1):
+    if i % 2 == 0:
+        sum_range3 += i
+print(sum_range3)
